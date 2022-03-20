8e65d02b009849ee24d8ce11c3473dedea313a5f
aleynayasar <79570388+aleynayasar@users.noreply.github.com>
2022-03-20 18:30:00 +0300

    Add files via upload

---
+++ Country.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAndSetGet
+{
+    class Country
+    {
+        private string firstName;
+        private string capital;
+        private int population;
+        private string currency;
+
+        public string FirstName 
+        {
+            get {return firstName;}  set { firstName = value.ToUpper(); }
+        }
+        public string Capital
+        {
+            get { return capital; } set { capital = value.ToUpper(); }
+        }
+        public int Population
+        {
+            get { return population; } set { population = value; }
+        }
+        public string Currency
+        {
+            get { return currency; }  set { currency = value; }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ClassAndSetGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country = new Country();
+            country.FirstName = "Semih";
+            country.Capital = "Turkey";
+            country.Population = 850000; 
+            country.Currency = "TL";
+
+            Console.WriteLine("First Name : "+ country.FirstName);
+            Console.WriteLine("The Capital : " + country.Capital);
+            Console.WriteLine("The Population : " + country.Population);
+            Console.WriteLine("Currency  : " + country.Currency);
+
+
+            Console.ReadKey();
+           
+        }
+    }
+}
